5157d1b717eeae7c087a63dbd048cebd3422f746
dillinger <o.shahovyi@gmail.com>
2021-10-03 23:24:07 +0300

    PyPi files

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+import animal_avatar
+
+setup(
+    name='animal_avatar',
+    version=animal_avatar.__version__,
+    description='SVG avatar generator.',
+    long_description=animal_avatar.__doc__,
+    author_email='johnherbertdillinger@ukr.net',
+    py_modules=['animal_avatar'],
+    scripts=['animal_avatar.py'],
+    license='MIT',
+    platforms='any',
+    url='https://github.com/Nacht1gall/animal-avatar-generator',
+    download_url='https://github.com/Nacht1gall/animal-avatar-generator/archive/refs/heads/master.zip',
+    keywords=['AVATAR', 'SVG'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
