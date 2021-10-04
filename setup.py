fc1d16e83e8e16fd2a3d101f7526b1c4607c0ffa
dillinger <o.shahovyi@gmail.com>
2021-10-04 08:50:00 +0300

    Updated the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='animal_avatar',
-    version='0.0.2',
+    version='0.0.3',
     description='SVG avatar generator.',
     long_description=
     """
